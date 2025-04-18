59a08fc1e0cf30668fd42f19d94edd83887542bf
Bogdan Floris <bogdan.floris@gmail.com>
2025-04-18 10:31:19 +0300

    feat: main cli app

--- src/main.c
+++ src/main.c
@@ -1,27 +1,59 @@
 #include "config.h"
+#include "network.h"
 
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-    char *config_filename = "topology.ini";
+int main(int argc, char *argv[]) {
+    char *config_filename;
     config_t config;
-    int result;
 
-    printf("Parsing network topology configuration: %s\n", config_filename);
-
-    init_config(&config);
-    result = parse_config_file(config_filename, &config);
-    if (result != 0) {
-        printf("ERROR: Failed to parse network topology configuration with "
-               "code %d\n",
-               result);
+    if (argc != 3) {
+        printf("Usage: %s <config_file> <--up|--down>\n", argv[0]);
         return EXIT_FAILURE;
     }
-    print_config(&config, stdout);
+
+    config_filename = argv[1];
+
+    init_config(&config);
+    int result = 0;
+    ;
+    if ((result = parse_config_file(config_filename, &config)) != 0) {
+        fprintf(stderr,
+                "ERROR: Failed to parse network topology configuration with "
+                "code %d\n",
+                result);
+        return EXIT_FAILURE;
+    }
+
+    int status = 0;
+    if (strcmp(argv[2], "--up") == 0) {
+        status = network_init(&config);
+        if (status != 0) {
+            fprintf(stderr,
+                    "ERROR: Failed to initialize network with code %d\n",
+                    status);
+            goto out_delete;
+        }
+    } else if (strcmp(argv[2], "--down") == 0) {
+        status = network_cleanup(&config);
+        if (status != 0) {
+            fprintf(stderr, "ERROR: Failed to clean up network with code %d\n",
+                    status);
+            goto out_delete;
+        }
+    } else {
+        fprintf(stderr, "Invalid argument: %s\n", argv[2]);
+        goto out_delete;
+    }
 
     free_config(&config);
 
     return 0;
+
+out_delete:
+    free_config(&config);
+    return EXIT_FAILURE;
 }
